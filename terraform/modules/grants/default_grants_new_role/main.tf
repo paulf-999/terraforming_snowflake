a5bd57b6585de03d26dd9651e6c08d3a618b6ceb
Paul Fry <paul-fry@hotmail.com>
2024-12-19 11:59:17 +0000

    added Terraform modules

--- terraform/modules/grants/default_grants_new_role/main.tf
+++ terraform/modules/grants/default_grants_new_role/main.tf
@@ -8,15 +8,15 @@ locals {
   ]
 }
 
-# Assign Snowflake's system roles as parent roles to `OPERATIONS_ALL_ROLE`
-resource "snowflake_grant_account_role" "grant_ops_all_role_to_system_roles" {
+# Assign Snowflake's system roles as parent roles to the input role
+resource "snowflake_grant_account_role" "grant_input_role_to_system_roles" {
   for_each         = toset(local.system_roles) # iterate through the entries in the list role_grants
   role_name        = var.role_name
   parent_role_name = each.value
 }
 
 # Allow the role to use the warehouse 'DEV_WH'
-resource "snowflake_grant_privileges_to_account_role" "grant_warehouse_usage_to_ops_all_role" {
+resource "snowflake_grant_privileges_to_account_role" "grant_warehouse_usage_to_input_role" {
 
   account_role_name = var.role_name
 
