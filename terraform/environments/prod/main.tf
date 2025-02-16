1bb04bc59b71f3c2dd1e6349007aa42ea385ecc1
Paul Fry <paul-fry@hotmail.com>
2025-02-16 21:04:40 +0000

    eg demo - simplify creation of PROD db

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -1,13 +1,3 @@
-# Roles & Grants
-module "roles_and_grants" {
-  source    = "./1_roles_and_grants"
-  providers = { snowflake = snowflake }
-
-  db_name_prod_db     = "PROD"
-}
-
-# Databases
-module "example_db" {
-  source    = "./2_account_level_objects/database/prod_db"
-  providers = { snowflake = snowflake }
+resource "snowflake_database" "db_prod" {
+  name = "PROD"
 }
