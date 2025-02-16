479ae6f675fcda4295c3fb47e0116f9239224d3c
Paul Fry <paul-fry@hotmail.com>
2025-02-16 20:56:01 +0000

    PR pipeline housekeeping - code deployments

--- src/cicd/scripts/code_deployment_scripts/push_files_to_git/functions_push_files_to_git.sh
+++ src/cicd/scripts/code_deployment_scripts/push_files_to_git/functions_push_files_to_git.sh
@@ -27,8 +27,8 @@ configure_git_user() {
 
 # Ensure we're on the correct branch
 checkout_branch() {
-    log_message "${DEBUG}" "Ensuring we're on branch: ${SOURCE_GIT_BRANCH_NAME}"
-    log_cmd_message "${DEBUG_DETAILS}" "cmd: git checkout -B ${SOURCE_GIT_BRANCH_NAME}"
+    log_message "${DEBUG}" 'Ensuring wee on branch: ${SOURCE_GIT_BRANCH_NAME}'
+    log_cmd_message "${DEBUG_DETAILS} "cmd: git checkout -B ${SOURCE_GIT_BRANCH_NAME}"
     git checkout -B "${SOURCE_GIT_BRANCH_NAME}" || {
         log_message "${ERROR}" "Error: Failed to checkout or create the branch ${SOURCE_GIT_BRANCH_NAME}."
         exit 1
@@ -37,7 +37,7 @@ checkout_branch() {
 
 # Fetch latest changes from remote Git branch
 fetch_remote() {
-    log_cmd_message "${DEBUG_DETAILS}" "cmd: git fetch origin >/dev/null 2>&1"
+    log_cmd_message "${DEBUG_DETAILS} "cmd: git fetch origin >/dev/null 2>&1"
     git fetch origin >/dev/null 2>&1 || {
         log_message "${ERROR}" "Error: Failed to fetch from remote."
         exit 1
@@ -46,33 +46,33 @@ fetch_remote() {
 
 # Merge changes from remote and resolve conflicts
 merge_remote_branch() {
-    log_cmd_message "${DEBUG_DETAILS}" "cmd: git merge -X ours origin/${SOURCE_GIT_BRANCH_NAME} --no-edit >/dev/null 2>&1"
+    log_cmd_message "${DEBUG_DETAILS} "cmd: git merge -X ours origin/${SOURCE_GIT_BRANCH_NAME} --no-edit >/dev/null 2>&1"
     git merge -X ours origin/"${SOURCE_GIT_BRANCH_NAME}" --no-edit >/dev/null 2>&1 || {
         log_message "${DEBUG}" "Merge conflict detected. Resolving conflicts..."
         resolve_git_merge_conflicts # see function below
     }
 }
 
-# Resolve Git merge conflicts
+# Resolve Git merge conflicts - I think this function is now redundant, but would need to be confirmed
 resolve_git_merge_conflicts() {
-    log_cmd_message "${DEBUG_DETAILS}" "Resolving merge conflicts. Cmd: git ls-files -u | awk '{ print \$4 }' | while read -r conflicted_file; do"
+    log_cmd_message "${DEBUG_DETAILS} "Resolving merge conflicts. Cmd: git ls-files -u | awk '{ print $4 }' | while read -r conflicted_file; do"
     git ls-files -u | awk '{ print $4 }' | while read -r conflicted_file; do
         if [[ "${conflicted_file}" != "${FILENAME}" ]]; then
             log_message "${DEBUG}" "Importing remote version for unedited file: ${conflicted_file}"
-            log_cmd_message "${DEBUG_DETAILS}" "cmd: git checkout --theirs ${conflicted_file}"
+            log_cmd_message "${DEBUG_DETAILS} "cmd: git checkout --theirs ${conflicted_file}"
             git checkout --theirs "${conflicted_file}"
-            log_cmd_message "${DEBUG_DETAILS}" "cmd: git add ${conflicted_file}"
+            log_cmd_message "${DEBUG_DETAILS} "cmd: git add "${conflicted_file}""
             git add "${conflicted_file}"
         else
             log_message "${DEBUG}" "Keeping local version for edited file: ${FILENAME}"
-            log_cmd_message "${DEBUG_DETAILS}" "cmd: git checkout --ours ${FILENAME}"
+            log_cmd_message "${DEBUG_DETAILS} "cmd: git checkout --ours ${FILENAME}"
             git checkout --ours "${FILENAME}"
-            log_cmd_message "${DEBUG_DETAILS}" "cmd: git add ${FILENAME}"
+            log_cmd_message "${DEBUG_DETAILS} "cmd: git add ${FILENAME}"
             git add "${FILENAME}"
         fi
     done
 
-    log_cmd_message "${DEBUG_DETAILS}" "cmd: git commit -m 'Resolved merge conflicts by keeping local or remote versions as needed.' >/dev/null"
+    log_cmd_message "${DEBUG_DETAILS} "cmd: git commit -m Resolved merge conflicts by keeping local or remote versions as needed. >/dev/null'"
     git commit -m "Resolved merge conflicts by keeping local or remote versions as needed."
     log_message "${DEBUG}" "Conflicts resolved."
 }
